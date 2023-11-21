4dfc80a65b149324e0c0c049dcdd686508eea00b
Maximilian Scheel <scheel.development@gmail.com>
2023-11-21 22:30:30 +0100

    Starting mobile SCSS

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,9 +9,8 @@ import { NgIf } from '@angular/common';
 })
 export class HomeComponent {
   images = [
-    { name: 'BER_Aktuell_1.jpg', caption: 'lorem ipsum 1 asdasdasdasdasdasdddddddddddddddddddddddddd'},
-    { name: 'BER_Aktuell_2.jpg', caption: 'caption2'},
-    { name: 'BER_Aktuell_3.jpg', caption: 'caption3'},
-    { name: 'BER_Aktuell_4.jpg', caption: 'caption4'},
+    { name: 'BER_Aktuell_1.jpg', caption: 'vorbereitung der erstbegehung - mit ideen im gepäck /// umbau einer scheune in letschin, derzeit lph 2'},
+    { name: 'BER_Aktuell_3.jpg', caption: 'sichtbeton-sockel und hinterlüftete holzlattung vertikal und horizontal /// wohnhaus mit 2 wohneinheiten in stralsund, derzeit lph 5 & 6'},
+    { name: 'BER_Aktuell_4.jpg', caption: ' monolith an der ecke /// erweiterung einer doppelhaushälfte in stralsund, derzeit lph 3'},
   ];
 }
