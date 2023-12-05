ff53e559e6545cb9f4fc72a8edb33940b87dfb2b
Maximilian Scheel <scheel.development@gmail.com>
2023-12-05 20:42:29 +0100

    Changing img bug

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,4 @@
-import { Component,  } from '@angular/core';
-import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -9,8 +7,8 @@ import { NgIf } from '@angular/common';
 })
 export class HomeComponent {
   images = [
-    { name: 'BER_Aktuell_1.jpg', caption: 'vorbereitung der erstbegehung - mit ideen im gepäck'},
-    { name: 'BER_Aktuell_3.jpg', caption: 'sichtbeton-sockel und hinterlüftete holzlattung vertikal und horizontal /// wohnhaus mit 2 wohneinheiten in stralsund, derzeit lph 5 & 6'},
-    { name: 'BER_Aktuell_4.jpg', caption: ' monolith an der ecke /// erweiterung einer doppelhaushälfte in stralsund, derzeit lph 3'},
+    { name: 'BER_Aktuell_1.jpg', caption: 'monolith an der ecke <br>/// erweiterung einer doppelhaushälfte in stralsund, derzeit lph 3' },
+    { name: 'BER_Aktuell_4.jpg', caption: 'sichtbeton-sockel und hinterlüftete holzlattung vertikal und horizontal <br>/// wohnhaus mit 2 wohneinheiten in stralsund, derzeit lph 5 & 6' },
   ];
 }
+
